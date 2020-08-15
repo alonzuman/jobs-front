18ebcb7634878993dce9b4ed139bbd3bc7385a1f
Alon Zuman <alonzuman7@gmail.com>
2020-08-15 14:58:29 +0300

    started moving dialogs to dialog context

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 import { IconButton } from '@material-ui/core'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { AuthContext } from '../contexts/Auth';
 
 const BackButton = () => {
+  const { theme } = useContext(AuthContext)
   const history = useHistory()
+  const backButtonStyle = {
+    margin: '1rem',
+    position: 'fixed',
+    zIndex: 9999,
+    backgroundColor: theme.palette.type === 'dark' ? '#333333' : 'white',
+    boxShadow: '0 0 10px #00000015'
+  }
 
-  return <IconButton onClick={() => history.goBack()}><ArrowBackIcon /></IconButton>
+  return <IconButton style={backButtonStyle} onClick={() => history.goBack()}><ArrowBackIcon /></IconButton>
 }
 
 export default BackButton
